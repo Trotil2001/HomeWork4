b886d3f38e80c558e7cafa3be21e3363e00e0814
Trotil_2001 <trotil_2001@mail.ru>
2022-09-13 22:35:55 +0800

    Задача 29

---
+++ Task29/Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+Задача 29: Напишите программу, которая задаёт массив из 8 
+элементов и выводит их на экран.
+
+1, 2, 5, 7, 19, 6, 1, 33 -> [1, 2, 5, 7, 19, 6, 1, 33]
+*/
+
+int[] inputArray()
+{
+    int[] arr=new int[8];
+    for (int i =0; i < arr.Length; i++)
+    {
+        Console.WriteLine("Введите " + (i+1) + "-ый элемент массива");
+        arr[i]=Convert.ToInt32(Console.ReadLine());
+    }
+    return arr;
+}
+
+void outputArray(int[] arr)
+{
+    Console.WriteLine();
+    Console.WriteLine("[" + string.Join(", ", arr)+ "]");
+}
+outputArray(inputArray());
