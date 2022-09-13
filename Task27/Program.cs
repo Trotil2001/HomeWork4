7c2f8c5665f2729a6aeea4869525b3c90fc63648
Trotil_2001 <trotil_2001@mail.ru>
2022-09-13 22:35:44 +0800

    Задача 27

---
+++ Task27/Program.cs
@@ -0,0 +1,28 @@
+﻿/*
+Задача 27: Напишите программу, которая принимает на вход 
+число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12
+
+*/
+
+void Sum(int number)
+{
+    int count = 0, LastDigit = 0, sum = 0;
+
+    if (number < 0)
+        number *= (-1);
+//Находим последнюю цифру числа и суммируем.
+    while (number > 0)
+    {
+        LastDigit = number % 10;//Последня цифра числа
+        sum += LastDigit;
+        number /= 10;
+        count++;
+    }
+    Console.WriteLine($"Сумма чисел = {sum}");
+}
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Sum(n);
